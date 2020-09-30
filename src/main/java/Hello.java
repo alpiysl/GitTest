a79ab59605b622e51cac8fed274b23923eb954be
Alpaslan Uysal <alpiysll@gmail.com>
2020-09-30 23:40:48 +0300

    uploaded

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,6 +2,7 @@ public class Hello {
 
     public static void main(String[] args) {
 
-        System.out.println("hello");
+        System.out.println("hello world");
+        //more changes are about to come
     }
 }
